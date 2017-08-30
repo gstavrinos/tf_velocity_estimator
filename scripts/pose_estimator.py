019a4c7aa79abfb0b200a556030c5b2e315779e9
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-08-30 21:02:10 +0300

    Tested additions. Printing latest velocity vector for now

--- scripts/pose_estimator.py
+++ scripts/pose_estimator.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 import tf
 import rospy
+import traceback
 from tf import TransformListener
 from tf2_msgs.msg import TFMessage
 from geometry_msgs.msg import PoseStamped
 from tf_pose_estimator.srv import EstimatorRequest
 
-tf_broadcaster = None
+tf_ = None
+targeted_tf = ''
 sliding_window = []
 sliding_window_v = []
-tf_ = None
+tf_broadcaster = None
 sliding_window_sz = 0
 
 def init():
     global tf_broadcaster, targeted_tf, tf_, sliding_window_sz
     rospy.init_node('tf_pose_estimator')
     targeted_tf = rospy.get_param("~targeted_tf", "helipad")
-    targeted_tf = rospy.get_param("~sliding_window_sz", 10)
+    sliding_window_sz = rospy.get_param("~sliding_window_sz", 10)
     tf_broadcaster = tf.TransformBroadcaster()
     tf_ = TransformListener()
     rospy.Subscriber("tf", TFMessage, tf_callback)
@@ -30,22 +32,28 @@ def approx(p1, p2):
     return False
 
 def tf_callback(tf2):
-    global tf_broadcaster, targeted_tf, tf_, sliding_window_sz, sliding_window, sliding_window_v
+    global tf_broadcaster, targeted_tf, tf_
+    global sliding_window_sz, sliding_window, sliding_window_v
     #if tf_.frameExists("odom") :#and tf_.frameExists(targeted_tf):
     try:
         t = tf_.getLatestCommonTime("/odom", targeted_tf)
         position, quaternion = tf_.lookupTransform("/odom", targeted_tf, t)
-        print position
         # Untested from here
         ps = PoseStamped()
         ps.header.stamp = rospy.Time.now()
         ps.header.frame_id = targeted_tf
-        ps.pose.position = position
-        ps.pose.orientation = quaternion
-        sliding_window.push_back(ps)
+        ps.pose.position.x = position[0]
+        ps.pose.position.y = position[1]
+        ps.pose.position.z = position[2]
+        ps.pose.orientation.x = quaternion[0]
+        ps.pose.orientation.y = quaternion[1]
+        ps.pose.orientation.z = quaternion[2]
+        ps.pose.orientation.w = quaternion[3]
+        sliding_window.append(ps)
         if len(sliding_window) >= sliding_window_sz:
             del sliding_window[0]
-            del sliding_window_v[0]
+            if len(sliding_window_v) >= sliding_window_sz:
+                del sliding_window_v[0]
         vx = 0
         vy = 0
         vz = 0
@@ -55,56 +63,18 @@ def tf_callback(tf2):
             dx = sliding_window[-1].pose.position.x - sliding_window[-2].pose.position.x
             dy = sliding_window[-1].pose.position.y - sliding_window[-2].pose.position.y
             dz = sliding_window[-1].pose.position.z - sliding_window[-2].pose.position.z
-            dt = sliding_window[-1].header.stamp - sliding_window[-2].header.stamp
+            dt = (sliding_window[-1].header.stamp.secs + sliding_window[-1].header.stamp.nsecs * 10e-9) - (sliding_window[-2].header.stamp.secs + sliding_window[-2].header.stamp.nsecs * 10e-9)
             vx = dx / dt
             vy = dy / dt
             vz = dz / dt
             v = [vx, vy, vz]
+            print v
             sliding_window_v.append(v)
         else:
             sliding_window_v.append(v)
         # Till here!
-
-
-    except:
-        pass
-    '''
-    for tf in tf2.transforms:
-    if tf.child_frame_id == targeted_tf:
-        tf_broadcaster.sendTransform(
-            (width / 2 + top_left_x, -height / 2 + top_left_y, 0),
-            (0, 0, 0, 1.0),
-            rospy.Time.now(),
-            'helipad',
-            'ar_marker_' + master_marker)
-
-        marker = Marker()
-        marker.header.frame_id = 'ar_marker_' + master_marker
-        marker.header.stamp = rospy.Time.now()
-        marker.ns = 'ar_helipad'
-        marker.id = 0
-        # Based on http://docs.ros.org/api/visualization_msgs/html/msg/Marker.html
-        # The shape code for the cube (rectangle) is 1
-        # The add/modify action is 0
-        marker.type = 1
-        marker.action = 0
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        marker.color.a = 1.0
-        marker.scale.x = width
-        marker.scale.y = height
-        marker.scale.z = 0.001
-        marker.pose.position.x = width / 2 + top_left_x
-        marker.pose.position.y = -height / 2 + top_left_y
-        marker.pose.position.z = 0
-        marker.pose.orientation.x = 0
-        marker.pose.orientation.y = 0
-        marker.pose.orientation.z = 0
-        marker.pose.orientation.w = 1.0
-        marker.lifetime = rospy.Duration(1);
-        marker_publisher.publish(marker)
-    '''
+    except Exception as e:
+        print traceback.format_exc()
 
 def pose_estimation(msg):
     print msg
